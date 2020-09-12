e5b8aa4e87d836daa82714f24dc8093ce6e0df0e
Antony Silva <antoydsi@gmail.com>
2020-09-12 13:00:08 -0300

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# TransactionType.create([
-#     {id: 1, name: "Débito", signal: "+"},
-#     {id: 2, name: "Boleto", signal: "-"},
-#     {id: 3, name: "Financiamento", signal: "-"},
-#     {id: 4, name: "Crédito", signal: "+"},
-#     {id: 5, name: "Recebimento Empréstimo", signal: "+"},
-#     {id: 6, name: "Vendas", signal: "+"},
-#     {id: 7, name: "Recebimento TED", signal: "+"},
-#     {id: 8, name: "Recebimento DOC", signal: "+"},
-#     {id: 9, name: "Aluguel", signal: "-"}
-# ]) 
+TransactionType.create([
+    {id: 1, name: "Débito", signal: "+"},
+    {id: 2, name: "Boleto", signal: "-"},
+    {id: 3, name: "Financiamento", signal: "-"},
+    {id: 4, name: "Crédito", signal: "+"},
+    {id: 5, name: "Recebimento Empréstimo", signal: "+"},
+    {id: 6, name: "Vendas", signal: "+"},
+    {id: 7, name: "Recebimento TED", signal: "+"},
+    {id: 8, name: "Recebimento DOC", signal: "+"},
+    {id: 9, name: "Aluguel", signal: "-"}
+]) 
 
-# User.create(name: 'Sample User', email: 'sample_user@gmail.com', password: 'password')
+User.create(name: 'Sample User', email: 'sample_user@gmail.com', password: 'password')
